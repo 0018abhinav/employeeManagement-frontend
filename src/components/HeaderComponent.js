ac1b49b8354ba2b2cdd1dd820f6ddd27ef1ec80c
Abhinav <abhinav0018.be20@chitkara.edu.in>
2024-07-23 12:36:06 +0530

    setting header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
             <header>
                 <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
                     <div>
-                        <a href = "https://javaguides.net" className = "navbar-brand">
+                        <a href = "https://github.com/0018abhinav/employeeManagement-frontend" className = "navbar-brand">
                             Employee Management Application
                         </a>
                     </div>
